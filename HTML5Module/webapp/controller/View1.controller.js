6440381930ccaf3f78ed04b3ea3d29a5a215793e
kevin <kgkitty@126.com>
2020-11-19 06:36:10 +0000

    done

---
+++ HTML5Module/webapp/controller/View1.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+		"sap/ui/core/mvc/Controller"
+	],
+	/**
+     * 
+     */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("ns.HTML5Module.controller.View1", {
+			onInit: function () {
+
+            },
+            alert:function(){
+                alert("kevin New")
+            }
+		});
+	});
